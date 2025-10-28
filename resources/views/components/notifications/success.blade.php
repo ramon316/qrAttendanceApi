a827cdb30ea8c021cb8d687e19247bba31dcfe23
Ramón Esteban Márquez García <ramon316@hotmail.com>
2025-10-28 08:09:44 -0600

    before hosting last details

---
+++ resources/views/components/notifications/success.blade.php
@@ -0,0 +1,12 @@
+<div class="bg-green-50 border-l-4 border-green-500 p-4 rounded-lg">
+    <div class="flex">
+        <div class="flex-shrink-0">
+            <i class="fa-solid fa-circle-check text-green-500 text-lg"></i>
+        </div>
+        <div class="ml-3">
+            <p class="text-sm text-green-700">
+                {{ $slot }}
+            </p>
+        </div>
+    </div>
+</div>
